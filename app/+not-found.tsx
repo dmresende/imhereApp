8c2fe709ed116e66410a595705e3d4779240974d
Seu Nome <seuemail@example.com>
2025-03-05 08:01:15 -0300

    fix: commit provisory

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,19 +1,17 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
+      <View style={styles.container}>
+        <View >This screen doesn't exist.</View>
+        {/* <Link href="/" style={styles.link}>
+          <View >Go to home screen!</View>
+        </Link> */}
+      </View>
     </>
   );
 }
